797513d46396a4d9079f605918d60195e1ff1552
Mauricio Matchal <mauriciomatchal@gmail.com>
2024-05-01 20:46:45 -0300

    Adição de responsividade

--- 2-PlantCard/plantcard.js
+++ 2-PlantCard/plantcard.js
@@ -3,7 +3,7 @@ let popupDrawer = document.getElementById("popup_drawer");
 
 function sharePopUp() {
   if (popupDrawer.style.visibility == "hidden") {
-    popupDrawer.style.visibility = "visible"; 
+    popupDrawer.style.visibility = "visible";
   } else {
     popupDrawer.style.visibility = "hidden";
   }
