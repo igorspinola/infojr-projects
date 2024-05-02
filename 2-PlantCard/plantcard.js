5f411f846e134b53593b25718ca43db9a37609ec
talitaester <talita.alves@ufba.br>
2024-05-02 12:01:42 -0300

    modified:   2-PlantCard/plantcard.css 	modified:   2-PlantCard/plantcard.html 	modified:   2-PlantCard/plantcard.js

--- 2-PlantCard/plantcard.js
+++ 2-PlantCard/plantcard.js
@@ -1,17 +1,8 @@
 const hamMenu = document.getElementById('hamburguer');
-const botaoMenu = document.getElementById('imagem4');
+var botaoMenu = document.getElementById('imagem4');
 
 function clickMenu() {
-    // Obtém o estilo computado do elemento
-    const computedStyle = window.getComputedStyle(hamMenu);
-
-    // Verifica se o elemento está sendo exibido ou oculto
-    if (computedStyle.display === "none") {
-        console.log("Olá, mundo!")
-        hamMenu.style.display = "flex";
-    } else {
-        hamMenu.style.display = "none";
-    }
+    console.log("Olá, mundo!")
 }
 
 botaoMenu.addEventListener("click", clickMenu);
