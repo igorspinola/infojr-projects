e4d91cfab497f41ebafff2ff472f608a5bb4d2dc
Marcello Lima da Conceição <marcelloconceicao@ufba.br>
2024-05-07 17:30:14 -0300

    Animação planejada, toques finais no CSS prontos, falta terminar o script do JS, há um bug com o DevTools

---
+++ 2-PlantCard/styles/script.js
@@ -0,0 +1,13 @@
+
+
+/* function triggerAnimation () {
+    document.getElementById("curtain").classList.add("animation");
+}; */
+
+let share_btn = document.getElementById("share");
+
+function conlog () {
+
+    console.log("Hello");
+};
+
