09fe032362cc181bad7293c0ba582a87351949a2
Beatriz Pereira Espinheira <beatrizespinheiraa@gmail.com>
2024-05-07 18:12:19 -0300

    adiciona animações e melhora acessibilidade html

--- 2-PlantCard/script.js
+++ 2-PlantCard/script.js
@@ -1,6 +1,18 @@
 const btn = document.getElementById('share');
 
-function toggleMenu(){
+function toggleMenu(event){
+
+    if (event.type==='touchstart') event.preventDefault();
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
+    const active = menu.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label','Abrir Menu')
+    }
 }
+
+btn.addEventListener('click', toggleMenu);
+btn.addEventListener('touchstart', toggleMenu);
