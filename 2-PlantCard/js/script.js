1c5e5975589fc02c74d451d27b37f27e3e8b8b1d
Pedro de Sá <teles.pedro97@gmail.com>
2024-05-10 08:09:21 +0000

    criação das pastas 'css' e 'js', e importação dos códigos em CSS e JavaScript iniciais

---
+++ 2-PlantCard/js/script.js
@@ -0,0 +1,8 @@
+function mostrarMenu() {
+    let hMenu = document.getElementById("h-menu");
+    if (hMenu.style.visibility === "hidden") {
+        hMenu.style.visibility = "visible";
+    } else {
+        hMenu.style.visibility = "hidden";
+    };
+};
