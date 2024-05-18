a4c2219b82e5d82fdb9d0d8aae0abf914da18572
Igor <igorspinoladev@gmail.com>
2024-05-18 03:11:26 -0300

    mobile added

--- 3-Portfolio/portfolio/src/App.jsx
+++ 3-Portfolio/portfolio/src/App.jsx
@@ -11,38 +11,44 @@ function App() {
 
   return (
     <div className="article">
-      <div className="avatar-div">
-        <img src={avatar} className="avatar" alt="avatar" />
-      </div>
 
-      <h1>Hello.</h1>
-      <h1>My name is {name}.</h1>
-      <hr/>
+      {/* DESKTOP */}
+      <div className="top">
 
-      <div className="card">
-
-        <p>
-          I'm a Digital Product and UI Designer – creating digital experiences that are intuitive for real people and making complex processes easy to use.
-        </p>
-        <p>
-          Right now, I'm Design Lead at <span>Holiday Extras</span>, covering all our digital platforms across a whole load of brands – leading a great team across <span>design</span>, UI engineering, accessibility and customer experience. I'm lucky to be part of a great team of designers, writers, engineers, PMs and data specialists – amongst others – testing, iterating and optimising our platforms and products.
-        </p>
-        <p>
-          I've got some <span>work on Dribbble</span>, some previous work at <span>Saga</span> and you can find me over on twitter and sometimes <span>on Medium too</span>. I also take <span>too many photos</span>.
-        </p>
-        
-        <div className="work">
-          <img src={circle}  alt="circle" />
-          <div>
-            Available for work and general design goodness – <span>say hello</span>
-          </div>
+        <div className="avatar-div">
+          <img src={avatar} className="avatar" alt="avatar" />
         </div>
 
+        <h1>Hello.</h1>
+        <h1>My name is {name}.</h1>
+        <hr/>
+
+        <div className="card">
+
+          <p>
+            I'm a Digital Product and UI Designer – creating digital experiences that are intuitive for real people and making complex processes easy to use.
+          </p>
+          <p>
+            Right now, I'm Design Lead at <span>Holiday Extras</span>, covering all our digital platforms across a whole load of brands – leading a great team across <span>design</span>, UI engineering, accessibility and customer experience. I'm lucky to be part of a great team of designers, writers, engineers, PMs and data specialists – amongst others – testing, iterating and optimising our platforms and products.
+          </p>
+          <p>
+            I've got some <span>work on Dribbble</span>, some previous work at <span>Saga</span> and you can find me over on twitter and sometimes <span>on Medium too</span>. I also take <span>too many photos</span>.
+          </p>
+          
+          <div className="work">
+            <img src={circle}  alt="circle" />
+            <div>
+              Available for work and general design goodness – <span>say hello</span>
+            </div>
+          </div>
+
+        </div>
+        <hr/>
+
       </div>
 
-      <hr/>
+      <div className="bottom">
 
-      <div className="the-container">
         <span className="title"><h3>Title</h3></span>
         <p className="title_text">
           Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. 
@@ -82,6 +88,88 @@ function App() {
         </div>
         
       </div>
+      {/* DESKTOP END */}
+
+      {/* MOBILE */}
+          <div className="top_mobile">
+
+            <div className="avatar-div">
+              <img src={avatar} className="avatar" alt="avatar" />
+              <h1>Hello.</h1>
+              <h1>My name is {name}.</h1>
+              <hr/>
+            </div>
+
+
+            <div className="card">
+
+              <p>
+                I'm a Digital Product and UI Designer – creating digital experiences that are intuitive for real people and making complex processes easy to use.
+              </p>
+              <p>
+                Right now, I'm Design Lead at <span>Holiday Extras</span>, covering all our digital platforms across a whole load of brands – leading a great team across <span>design</span>, UI engineering, accessibility and customer experience. I'm lucky to be part of a great team of designers, writers, engineers, PMs and data specialists – amongst others – testing, iterating and optimising our platforms and products.
+              </p>
+              <p>
+                I've got some <span>work on Dribbble</span>, some previous work at <span>Saga</span> and you can find me over on twitter and sometimes <span>on Medium too</span>. I also take <span>too many photos</span>.
+              </p>
+              
+              <div className="work">
+                <img src={circle}  alt="circle" />
+                <div>
+                  Available for work and general design goodness – <span>say hello</span>
+                </div>
+              </div>
+
+            </div>
+
+            <hr/>
+
+          </div>
+
+          <div className="bottom_mobile">
+
+            <span className="title"><h3>Title</h3></span>
+            <p className="title_text">
+              Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. Line of copy in here to decribe this section. 
+            </p>
+
+            <div className="image_container">
+
+              <img src={image} className="image" alt="image" />
+
+              <span className="one">
+                <h3>Title</h3>
+                <p>
+                  Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.
+                </p>
+                <span>
+                  Button Primary Light
+                </span>
+
+              </span>
+
+            </div>
+
+            <div className="image_container">
+              
+              <img src={image} className="image" alt="image" />
+
+              <span className="two">
+                <h3>Title</h3>
+                <p>
+                  Leading, implementing and evolving engaging customer experiences and UI foundations for every touch-point across various platforms.
+                </p>
+                <span>
+                  Button Primary Light
+                </span>
+              </span>
+
+
+            </div>
+            
+          </div>
+          {/* MOBILE END */}
+
 
     </div>
   )
