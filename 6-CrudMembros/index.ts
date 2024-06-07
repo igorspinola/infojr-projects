b784e0dd4b8aa314fc1ac07d82f77b916cf74a51
Igor <igorspinoladev@gmail.com>
2024-06-07 18:58:44 -0300

    user model fields added

--- 6-CrudMembros/index.ts
+++ 6-CrudMembros/index.ts
@@ -1,18 +1,42 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
+import {configDotenv} from "dotenv"
 import type { Request, Response } from "express"
+import { PrismaClient } from '@prisma/client';
+
+configDotenv()
+
+const prisma = new PrismaClient()
 
 const express = require('express')
 const app = express()
 const port = 3000
 
+// rota de retornar todos os usuarios GET
+
+// rota para criar um usuario POST
+app.post('/user', async (req: Request, res: Response) => {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Liandra',
+      email: 'leandrasantosadnv@gmail.com',
+      age: 20,  
+      password: "1234",
+      city: "Feira",
+      state: "Bahia"
+    },
+  }) 
+  console.log(user)
+  res.send(user)
+})
+
 app.get('/test', (req: Request, res: Response) => {
   console.log(req.query)
   res.send(
-  {  
-   data: 'Hello World!'
-  }
+    {  
+     data: 'Hello World!'
+    }
   )
 })
 
