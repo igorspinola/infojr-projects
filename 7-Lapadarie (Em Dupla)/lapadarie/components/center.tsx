7464079ff08446bd54f35ac4558435ca2c4220c0
Gabriel <gabriellealoliveira0@gmail.com>
2024-06-11 15:52:17 -0300

    icones adicionados

--- 7-Lapadarie (Em Dupla)/lapadarie/components/center.tsx
+++ 7-Lapadarie (Em Dupla)/lapadarie/components/center.tsx
@@ -1,3 +1,8 @@
+import IconePessoas from '../img/iconepessoas.png'
+import CarrinhoCompras from '../img/carrinhoCompras.png'
+import IconeDinheiro from '../img/IconeDinheiro.png'
+import Image from 'next/image'
+
 export default function center(){
     return(
         <div className="center">
@@ -5,12 +10,15 @@ export default function center(){
                 <div className="quadros">
                 <div className="NaFila">
                 <h5>Pessoas na fila</h5>
+                <Image src={IconePessoas} alt=""></Image>
                 </div>
                 <div className="paesVendidos">
                 <h5>Pães vendidos</h5>
+                <Image src={CarrinhoCompras} alt=""></Image>
                 </div>
                 <div className="entrada">
                     <h5>Entrada</h5>
+                    <Image src={IconeDinheiro} alt=""></Image>
                 </div>
                 </div>
             </div>
