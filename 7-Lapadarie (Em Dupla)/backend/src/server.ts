b292a0f3f55d2d85357eabb66671eeb65b5dcfa6
Igor <igorspinoladev@gmail.com>
2024-06-12 17:21:19 -0300

    add person route created

--- 7-Lapadarie (Em Dupla)/backend/src/server.ts
+++ 7-Lapadarie (Em Dupla)/backend/src/server.ts
@@ -1,8 +1,13 @@
+import { PrismaClient } from "@prisma/client"
 import { Request, Response } from "express"
+
+const prisma = new PrismaClient()
 
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.json());
 
 app.get('/test', (req: Request, res: Response) => {
   res.send('Hello World!')
@@ -10,4 +15,26 @@ app.get('/test', (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+// adicionar pessoa na fila
+
+app.post('/person', async (req: Request, res: Response) => {
+  console.log(req.body)
+  const {name, bread_total} = req.body
+  const person = await prisma.person.create({
+    data: 
+    {
+      name: name,
+      bread_total: parseInt(bread_total)
+    } 
+  })  
+  console.log(person)
+  
+  res.send(
+    {
+      message: "person added succesfully",
+      person: person
+    }   
+  )
 })
