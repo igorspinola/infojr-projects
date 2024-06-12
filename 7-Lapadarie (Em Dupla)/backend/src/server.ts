81f4d18fe8cbf6943b651da730cc64a20f34ca3a
Igor <igorspinoladev@gmail.com>
2024-06-12 19:27:30 -0300

    some routes added

--- 7-Lapadarie (Em Dupla)/backend/src/server.ts
+++ 7-Lapadarie (Em Dupla)/backend/src/server.ts
@@ -17,6 +17,20 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+// get paes vendidos
+
+// get numero de pessoas na fila 
+
+// get dinheiro que entrou
+
+
+// retornar pessoas da fila
+
+app.get('/line', async (req: Request, res: Response) => {
+  const people = await prisma.person.findMany()
+  res.send(people)
+})
+
 // adicionar pessoa na fila
 
 app.post('/person', async (req: Request, res: Response) => {
@@ -24,13 +38,23 @@ app.post('/person', async (req: Request, res: Response) => {
   const {name, bread_total} = req.body
   const person = await prisma.person.create({
     data: 
-    {
-      name: name,
-      bread_total: parseInt(bread_total)
-    } 
+      {
+        name: name,
+        bread_total: parseInt(bread_total)
+      } 
   })  
   console.log(person)
   
+  const dashboard = await prisma.dashboard.findUnique({
+    where: {
+      id: 1
+    }
+  }) || {id: 1, people_on_line: 0, breads_sold: 0, revenue: 0}
+
+  const new_people_on_line = dashboard.people_on_line + 1
+  const new_breads_sold = dashboard.breads_sold + 1
+  const new_revenue = new_breads_sold * 0.5 + dashboard.revenue
+
   res.send(
     {
       message: "person added succesfully",
