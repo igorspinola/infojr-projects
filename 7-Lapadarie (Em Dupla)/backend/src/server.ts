9972e930216a7de5cf1088d7c106711e8bd5757a
Igor <igorspinoladev@gmail.com>
2024-06-14 18:03:48 -0300

    backend finished

--- 7-Lapadarie (Em Dupla)/backend/src/server.ts
+++ 7-Lapadarie (Em Dupla)/backend/src/server.ts
@@ -17,15 +17,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-// get paes vendidos
-
-// get numero de pessoas na fila 
-
-// get dinheiro que entrou
-
-
-// retornar pessoas da fila
-
 app.get('/line', async (req: Request, res: Response) => {
   const people = await prisma.person.findMany()
   res.send(people)
@@ -46,14 +37,35 @@ app.post('/person', async (req: Request, res: Response) => {
   console.log(person)
   
   const dashboard = await prisma.dashboard.findUnique({
-    where: {
+    where: 
+    {
       id: 1
     }
   }) || {id: 1, people_on_line: 0, breads_sold: 0, revenue: 0}
 
   const new_people_on_line = dashboard.people_on_line + 1
-  const new_breads_sold = dashboard.breads_sold + 1
-  const new_revenue = new_breads_sold * 0.5 + dashboard.revenue
+  const new_breads_sold = dashboard.breads_sold + person.bread_total
+  const new_revenue = parseFloat((new_breads_sold * 0.5).toFixed(2))
+  
+  const updateDashboard = await prisma.dashboard.upsert({
+    where: 
+    {
+      id: 1
+    },
+    update: 
+    {
+      people_on_line: new_people_on_line,
+      breads_sold: new_breads_sold,
+      revenue: new_revenue
+    },
+    create: 
+    {
+      id: 1,
+      people_on_line: 1,
+      breads_sold: person.bread_total,
+      revenue: person.bread_total * 0.5
+    }
+  })
 
   res.send(
     {
@@ -61,4 +73,45 @@ app.post('/person', async (req: Request, res: Response) => {
       person: person
     }   
   )
+
 })
+
+// retirar pessoa da fila
+
+app.delete('/remove/:id', async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
+  
+  const removePerson = await prisma.person.delete({
+    where: {
+      id: id
+    }
+  })
+  const dashboard = await prisma.dashboard.findUnique({
+    where: 
+    {
+      id: 1
+    }
+  }) || {id: 1, people_on_line: 0, breads_sold: 0, revenue: 0}
+
+  const new_people_on_line = dashboard.people_on_line - 1
+
+  const updateDashboard = await prisma.dashboard.update({
+    where: 
+    {
+      id: 1
+    },
+    data: 
+    {
+      people_on_line: new_people_on_line 
+    },
+  })
+
+  res.send(
+    {
+      message: "person removed from line succesfully",
+      person: removePerson
+    }   
+  )
+
+})
+
